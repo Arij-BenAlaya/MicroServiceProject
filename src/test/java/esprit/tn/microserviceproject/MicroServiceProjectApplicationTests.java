69f4e2ba52bb21ea9f75963b6b2c721d8b3336d4
Arij-BenAlaya <arijbenalaya@esprit.tn>
2025-04-04 14:57:24 +0100

    configuration du projet

---
+++ src/test/java/esprit/tn/microserviceproject/MicroServiceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package esprit.tn.microserviceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
